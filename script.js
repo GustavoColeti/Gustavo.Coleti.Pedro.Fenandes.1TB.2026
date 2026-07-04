5b8c83ea239a6bd2833a279e7b31381e22dbc565
GustavoColeti <gustavo.coleti@escola.pr.gov.br>
2026-07-04 15:15:38 -0300

    hello

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggle(id) {
+  const el = document.getElementById(id);
 
+  if (el.style.display === "block") {
+    el.style.display = "none";
+  } else {
+    el.style.display = "block";
+  }
+}
